50b605ae4dddf331f315a530bbe722f2a16e101c
Ananya1928 <kodancha.ananya@gmail.com>
2025-07-01 23:12:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List, Optional
 
-__version__ = "24.3.1"
-
 
 def main(args: Optional[List[str]] = None) -> int:
-    """This is an internal API only meant for use by pip's own console scripts.
+    """This is preserved for old console scripts that may still be referencing
+    it.
 
     For additional details, see https://github.com/pypa/pip/issues/7498.
     """
